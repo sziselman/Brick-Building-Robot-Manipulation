686f8a6f443dfc240b94210b6842e081790a1390
sziselman <sarahziselman2020@u.northwestern.edu>
2021-04-11 19:49:03 -0500

    working on start service

--- trect2/launch/trect2_launch.py
+++ trect2/launch/trect2_launch.py
@@ -1,25 +1,33 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    return LaunchDescription([
-
+        ld = LaunchDescription()
+        
         config = os.path.join(
-            get_package_share_directory('trect2'), 'config', 'trect_params.yaml'),
+            get_package_share_directory('trect2'),
+            'config',
+            'trect_params.yaml'
+            )
 
-        Node(
+        trect2_node = Node(
             package='trect2',
-            namespace='trect2',
             executable='trect2',
             name='trect2',
-            parameters=[config]
-        ),
-        
-        Node(
-            package='turtlesim',
-            namespace='trect2',
-            executable='turtlesim_node',
-            name='sim'
+            parameters=[config],
+            output='screen'
         )
-    ])
 
+        turtlesim_node = Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='sim',
+            output='screen'
+        )
+
+        ld.add_action(trect2_node)
+        ld.add_action(turtlesim_node)
+
+        return ld
--- trect2/src/turtle_rect2.cpp
+++ trect2/src/turtle_rect2.cpp
@@ -15,27 +15,84 @@
 ///     draws the desired trajectory in yellow and causes the robot to follow the path
 ///     (path in lavender), provides the location and dimensions of the rectangle to the node.
 
+#include <chrono>
 #include "rclcpp/rclcpp.hpp"
+#include "trect2/srv/start.hpp"
 
 #include <turtlesim/msg/pose.hpp>
+
 #include <turtlesim/srv/set_pen.hpp>
 #include <turtlesim/srv/teleport_relative.hpp>
 #include <turtlesim/srv/teleport_absolute.hpp>
 #include <turtlesim/msg/color.hpp>
+
 #include <std_srvs/srv/empty.hpp>
 
 #include <geometry_msgs/msg/twist.hpp>
 
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+class trect2 : public rclcpp::Node
+{
+    public:
+        trect2() : Node("trect2")
+        {
+            posePub_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
+            twistSub_ = this->create_subscription<turtlesim::msg::Pose>("turtle1/pose", 10, std::bind(&trect2::poseCallback, this, _1));
+            startServ_ = this->create_service<trect2::srv::start>("/start", &start);
+            
+            this->declare_parameter("max_xdot");
+            this->declare_parameter("max_wdot");
+            this->declare_parameter("frequency");
+
+            this->get_parameter("max_xdot", max_xdot);
+            this->get_parameter("max_wdot", max_wdot);
+            this->get_parameter("frequency", frequency);
+
+            timer_ = this->create_wall_timer(500ms, std::bind(&trect2::timer_callback, this));
+        }
+
+    private:
+        double max_xdot, max_wdot, width, height;
+        int frequency;
+        turtlesim::msg::Pose turtle_pose;
+        geometry_msgs::msg::Twist twist_msg;
+
+        enum State {Idle, bottom, top, left, right, Rotate};
+        State currentState, previousState;
+
+        void poseCallback( turtlesim::msg::Pose::SharedPtr pose) 
+        {
+            turtle_pose = *pose;
+        }
+
+        void timer_callback()
+        {
+            twist_msg.linear.x = 1.0;
+            twist_msg.angular.z = 0;
+            posePub_->publish(twist_msg);
+        }
+
+        void start(const std::shared_ptr<trect2::srv::start::Request> req, std::shared_ptr<trect2::srv::start::Response> res)
+        {
+            width = req.width;
+            height = req.height;
+
+        }
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr posePub_;
+        rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr twistSub_;
+        rclcpp::Service<trect2::srv::start>::SharedPtr startServ_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        
+};
+
 int main(int argc, char * argv[])
 {
-    /************
-     * Initialize the node
-     * *********/
-    rclpp::init(argc, argv);
-    rclcpp::Node("trect2");
-
-    /************
-     * Initialize local variables
-     * *********/
-    double max_xdot, max_wdot, linVel, angVel;
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<trect2>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
