5628464ce64ecbed1d650abc794c3e24aa6e08a1
sziselman <sarahziselman2020@u.northwestern.edu>
2021-05-05 14:48:00 -0500

    update launch file

--- control/README.md
+++ control/README.md
@@ -2,11 +2,7 @@
 Package used for the controls of the HDT Adroit Manipulator Arm
 
 __Example Usage:__
-First, we want to launch the HDT Adroit Manipulator Arm software. It is important to note that the ```hdt_6dof_a24_pincer``` is needed to run the ```control``` package.
-```
-roslaunch hdt_6dof_a24_pincer_bringup hdt_arm_bringup_1.launch simulation:=true
-```
-Next, we want to launch the ```control``` package for the Adroit arm.
+First, we want to launch the ```control``` package for the Adroit arm.
 ```
 roslaunch control control.launch
 ```
--- control/src/control.cpp
+++ control/src/control.cpp
@@ -19,7 +19,7 @@
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
 #include <geometry_msgs/Pose.h>
-
+#include <string>
 
 int main(int argc, char* argv[])
 {
@@ -28,9 +28,6 @@ int main(int argc, char* argv[])
     ros::init(argc, argv, "arm_control");
     ros::NodeHandle n;
 
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
-    
     // Declare local variables
 
     ros::Rate loop_rate(100);
@@ -42,38 +39,70 @@ int main(int argc, char* argv[])
 
     // Define publishers, subscribers, services and clients
 
+
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    
+
     // Planning Groups
-    static const std::string arm_planning_group = "arm";
-    static const std::string pincer_planning_group = "pincer";
+    static const std::string PLANNING_GROUP = "arm";
 
     // Set up the Move Group Interface using the planning group
-    moveit::planning_interface::MoveGroupInterface arm_move_group_interface(arm_planning_group);
-    moveit::planning_interface::MoveGroupInterface pincer_move_group_interface(pincer_planning_group);
+    moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
 
     // Add collision objects in virtual world scene
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-    // Raw pointers used to refer to the planning group
-    const moveit::core::JointModelGroup* arm_model_group = arm_move_group_interface.getCurrentState()->getJointModelGroup(arm_planning_group);
-    const moveit::core::JointModelGroup* pincer_model_group = pincer_move_group_interface.getCurrentState()->getJointModelGroup(pincer_planning_group);
+    // // Raw pointers used to refer to the planning group
+    // const moveit::core::JointModelGroup* arm_model_group = arm_move_group_interface.getCurrentState()->getJointModelGroup(arm_planning_group);
+    // const moveit::core::JointModelGroup* pincer_model_group = pincer_move_group_interface.getCurrentState()->getJointModelGroup(pincer_planning_group);
 
     // // Visualization (using MoveItVisualTools)
     // namespace rvt = rviz_visual_tools;
-    // moveit_visual_tools::MoveItVisualTools visual_tools("")
+    // moveit_visual_tools::MoveItVisualTools visual_tools("joint1");
+    // visual_tools.deleteAllMarkers();
+
+    // visual_tools.trigger();
+
+
+
+
+    // /*************************
+    //  * Getting Basic Information
+    //  * **********************/
+
+    // ROS_INFO_STREAM("Getting basic information");
+
+    // // We can print the name of the reference frame for this robot.
+    // ROS_INFO_NAMED("tutorial", "Arm planning frame: %s", arm_move_group_interface.getPlanningFrame().c_str());
     
-    // Plan a motion for this group to a desired pose for the end-effector
-    geometry_msgs::Pose pincer_pose;
-    pincer_pose.orientation.w = 1.0;
-    pincer_pose.position.x = 1.0;
-    pincer_pose.position.y = 1.0;
-    pincer_pose.position.z = 1.0;
-    pincer_move_group_interface.setPoseTarget(pincer_pose);
+    // // We can also print the name of the end-effector link for this group.
+    // ROS_INFO_NAMED("tutorial", "Arm end effector link: %s", arm_move_group_interface.getEndEffectorLink().c_str());
+    
+    // // We can get a list of all the groups in the robot:
+    // ROS_INFO_NAMED("tutorial", "Available Planning Groups:");
+    // std::copy(arm_move_group_interface.getJointModelGroupNames().begin(),
+    //           arm_move_group_interface.getJointModelGroupNames().end(), std::ostream_iterator<std::string>(std::cout, ", "));
 
-    // Call the planner to compute the plan and visualize it
-    moveit::planning_interface::MoveGroupInterface::Plan pincer_plan;
-    bool success = (pincer_move_group_interface.plan(pincer_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    // ROS_INFO_STREAM("Finished getting basic information");
 
-    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+    // /*************************
+    //  * Start Motion Planning
+    //  * **********************/
+
+    // // Plan a motion for this group to a desired pose for the end-effector
+    // geometry_msgs::Pose arm_pose;
+    // arm_pose.orientation.w = 1.0;
+    // arm_pose.position.x = 1.0;
+    // arm_pose.position.y = 1.0;
+    // arm_pose.position.z = 1.0;
+    // pincer_move_group_interface.setPoseTarget(arm_pose);
+
+    // // Call the planner to compute the plan and visualize it
+    // moveit::planning_interface::MoveGroupInterface::Plan arm_plan;
+    // bool success = (arm_move_group_interface.plan(arm_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    // ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
 
     // while (ros::ok())
     // {
